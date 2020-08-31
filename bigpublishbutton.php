c95f6c4c5b720ac8ccc55a3b69cfa0fab70f7ccb
Gaetano Vallarelli <gaetanovallarelli@MacBook-Pro.local>
2020-08-31 14:17:59 +0200

    Added plugin description

--- bigpublishbutton.php
+++ bigpublishbutton.php
@@ -1,5 +1,16 @@
 <?php
+
+/**
+ * Plugin Name: Big Publish Button
+ * Description: Create a butto to deploy websites
+ * Version: 1.1.3
+ * Author: Biro
+ * Author URI: https://github.com/biroplane
+ */
+
+
 require_once( 'BigButtonUpdater.php' );
 if ( is_admin() ) {
     new BigButtonUpdater( __FILE__, 'biroplane', "bigpublishbutton" );
 }
+
